c0f02042adc3883d765267f60671e5669c404d38
joaovpt <joaotassi18@gmail.com>
2025-06-25 18:29:02 -0300

    chore: update main menu options and enhance exception handling for wallet operations

--- src/main/java/br/com/dio/Main.java
+++ src/main/java/br/com/dio/Main.java
@@ -2,16 +2,15 @@ package br.com.dio;
 
 import br.com.dio.exception.AccountNotFoundException;
 import br.com.dio.exception.NoFundsEnoughException;
+import br.com.dio.exception.WalletNotFoundException;
 import br.com.dio.model.AccountWallet;
 import br.com.dio.repository.AccountRepository;
 import br.com.dio.repository.InvestmentRepository;
 
 import java.util.Arrays;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 import static java.time.format.DateTimeFormatter.ISO_DATE_TIME;
-import static java.time.temporal.ChronoUnit.SECONDS;
 
 public class Main {
 
@@ -26,7 +25,7 @@ public class Main {
             System.out.println("Selecione a operação desejada:");
             System.out.println("1 - Criar uma conta");
             System.out.println("2 - Criar um investimento");
-            System.out.println("3 - Fazer um investimento");
+            System.out.println("3 - Criar uma carteira de investimento");
             System.out.println("4 - Depositar na conta");
             System.out.println("5 - Sacar da conta");
             System.out.println("6 - Transferencia entre contas");
@@ -41,24 +40,24 @@ public class Main {
             System.out.print("Opção: ");
             var option = scanner.nextInt();
             switch (option) {
-                case 1: createAccount();
-                case 2: createInvestment();
-                case 3: createWalletInvestment();
-                case 4: deposit();
-                case 5: withdraw();
-                case 6: transferToAccount();
-                case 7: incInvestment();
-                case 8: rescueInvestment();
-                case 9: accountRepository.list().forEach(System.out::println);
-                case 10: investmentRepository.list().forEach(System.out::println);
-                case 11: investmentRepository.listWallets().forEach(System.out::println);
-                case 12: {
+                case 1 -> createAccount();
+                case 2 -> createInvestment();
+                case 3 -> createWalletInvestment();
+                case 4 -> deposit();
+                case 5 -> withdraw();
+                case 6 -> transferToAccount();
+                case 7 -> incInvestment();
+                case 8 -> rescueInvestment();
+                case 9 -> accountRepository.list().forEach(System.out::println);
+                case 10 -> investmentRepository.list().forEach(System.out::println);
+                case 11 -> investmentRepository.listWallets().forEach(System.out::println);
+                case 12 -> {
                     investmentRepository.updateAmount();
                     System.out.println("Investimentos reajustados!");
                 }
-                case 13: checkHistory();
-                case 14: System.exit(0);
-                default:
+                case 13 -> checkHistory();
+                case 14 -> System.exit(0);
+                default ->
                     System.out.println("Opção inválida, tente novamente.");
             }
         }
@@ -137,7 +136,7 @@ public class Main {
         var amount = scanner.nextLong();
         try {
             investmentRepository.deposit(pix, amount);
-        } catch (AccountNotFoundException ex) {
+        } catch (WalletNotFoundException | AccountNotFoundException ex) {
             System.out.println(ex.getMessage());
         }
 
